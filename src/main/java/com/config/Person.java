a8242a7132b05b76720fb43644b0ec10a6de532d
sunyb <sunyb@bestinvestor.com>
2019-08-22 13:56:02 +0800

    配置信息加载

---
+++ src/main/java/com/config/Person.java
@@ -0,0 +1,55 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @autor sunyiban
+ * @date 2019/8/22 11:45
+ * @descrpition
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+
+    public String name;
+
+    public String age;
+
+    public String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import com.config.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/", method = RequestMethod.GET)
 public class IndexController {
 
+    @Autowired
+    private Person person;
 
     @RequestMapping("/index")
     public String index(String name) {
+        System.out.println(person.toString());
+
         if (name != null) {
             throw new RuntimeException("this is a test exception");
         }
